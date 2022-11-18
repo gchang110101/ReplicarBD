f8cfe34debccbeb0260ba48ceb39722ec026c719
Gabriel Chang <gdchang@uamv.edu.ni>
2022-11-18 16:37:21 -0600

    [ADD] primer (posiblemente final) avance

---
+++ src/entidades/Autor.java
@@ -0,0 +1,53 @@
+package entidades;
+
+/**
+ * @author Gabriel Chang
+ * @version 1.0
+ * @created 17-nov.-2022 8:24:02
+ */
+public class Autor {
+
+	private int authorID;
+	private String firstName;
+	private String lastName;
+
+	public Autor(){
+
+	}
+
+    public Autor(int authorID, String firstName, String lastName) {
+        this.authorID = authorID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+        
+        
+
+    public void finalize() throws Throwable {
+
+    }
+
+    public int getAuthorID() {
+        return authorID;
+    }
+
+    public void setAuthorID(int authorID) {
+        this.authorID = authorID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}//end Autor
